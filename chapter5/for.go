7705a485eeee45104963a55173f41e169c341b06
Shivshankar Kumar <shivshankar8w@gmail.com>
2025-12-03 23:14:11 +0530

    for loop update

--- chapter5/for.go
+++ chapter5/for.go
@@ -1,5 +1,29 @@
 package main
 
-func main(){
-	
+import "fmt"
+
+// only for exists in go for looping
+func main() {
+	// while loop
+	// i:=1
+	// for i<=5{
+	// 	fmt.Println(i)
+	// 	i+=1
+	// }
+
+	// infinite loop
+	// for {
+	// 	println("shiv")
+	// }
+
+	// classic for loop
+	// for i := 0; i <= 6; i++ {
+	// 	// break
+	// 	// continue
+	// 	fmt.Println(i)
+	// }
+
+	for i:= range 10{
+		fmt.Println(i)
+	}
 }
