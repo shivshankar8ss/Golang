e77603e71f70cb11c2bb3551ed0ca244ab710e04
Shivshankar Kumar <shivshankar8w@gmail.com>
2025-12-04 17:32:53 +0530

    arrays completed in Go

---
+++ chapter8/arrays.go
@@ -0,0 +1,27 @@
+package main
+
+func main() {
+	// var nums [5]int
+	// nums[0] = 12
+	// fmt.Println(nums[0])
+	// fmt.Println(len(nums))
+	// fmt.Println(nums)
+
+	// var vals [4]bool
+	// vals[1] = true
+	// fmt.Println(vals)
+
+	// var name [10]string
+	// name[1] = "shivshankar"
+	// fmt.Println(name)
+
+	// nums := [5]int{1, 2, 3, 4, 5}
+	// fmt.Println(nums)
+
+	// 2 d array
+
+	// nums := [2][2]int{{2, 3}, {8, 9}}
+	// fmt.Println(nums)
+
+	//    we used generally arrays for fixed size, memory optimizaation and constant time access
+}
